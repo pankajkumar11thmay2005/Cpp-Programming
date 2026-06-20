79877951a28c4d8da576b3e2ddaaddf608c0a16f
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-06-20 16:57:09 +0530

    Add files via upload

---
+++ Qs Set I/1stAreaofCircle.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float r = 3.5, area = 3.14 * r * r;
+  cout<<"Area of Circle: "<<area;
+}
---
+++ Qs Set I/SimpleInterest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int r = 4, p = 3000, t = 5;
+  int s = (p * r * t) / 100;
+  cout<<"Simple Interest: "<<s;
+}
