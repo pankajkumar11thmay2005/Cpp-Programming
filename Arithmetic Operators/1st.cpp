eadaed1576a6ad3eba40a6bdd84dd04bfca0e57f
Pankaj Kumar <pankajkumar11thmay2005@gmail.com>
2026-06-20 16:56:10 +0530

    Add files via upload

---
+++ Arithmetic Operators/1st.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 7, y = 2;
+  cout<<x + y<<endl;  // 7 + 2 = 9
+  cout<<x - y<<endl;  // 7 - 2 = 5
+  cout<<x * y<<endl;  // 7 * 2 = 14
+  cout<<x / y<<endl;  // 7 / 2 = 3 (integer division)
+  cout<<x % y<<endl;  // 7 % 2 = 1 (remainder)
+
+}
---
+++ Arithmetic Operators/2nd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float x = 7, y = 2;
+  cout<<x + y<<endl;  // 7 + 2 = 9
+  cout<<x - y<<endl;  // 7 - 2 = 5
+  cout<<x * y<<endl;  // 7 * 2 = 14
+  cout<<x / y<<endl;  // 7 / 2 = 3.5
+ 
+}
